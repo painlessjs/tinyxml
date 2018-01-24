1ebfe28b4b9a0055f1a8bab4d0078769767e0576
pixcai <pixcai@163.com>
2018-01-24 16:32:15 +0800

    Add function definitions

---
+++ src/index.js
@@ -0,0 +1,23 @@
+export function xml2js() {
+
+}
+
+export function xml2jsSync() {
+
+}
+
+export function js2xml() {
+
+}
+
+export function js2xmlSync() {
+
+}
+
+module.exports = {
+  xml2js,
+  xml2jsSync,
+  js2xml,
+  js2xmlSync
+};
+
